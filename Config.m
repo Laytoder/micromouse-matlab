6fb8cca6375c029a31ff8c7f578c2259f673e906
Lakshya Gupta <laytoder@gmail.com>
2021-12-01 20:04:47 -0700

    minor changes

--- Config.m
+++ Config.m
@@ -1,12 +1,17 @@
 classdef Config
     properties (Constant = true)
-        BRICK_NAME = 'CHPY';
-        LEFT_MOTOR = 'B'
-        RIGHT_MOTOR = 'C'
-        LIFT_MOTOR = 'A'
-        LIGHT_SENSOR = 1
-        LEFT_ULTRASONIC = 2
-        FRONT_ULTRASONIC = 4
-        RIGHT_ULTRASONIC = 3
+        %BRICK_NAME = 'CHPY';
+        BRICK_NAME = 'LEGO';
+        LEFT_MOTOR = 'B';
+        RIGHT_MOTOR = 'C';
+        LIFT_MOTOR = 'A';
+        %LIGHT_SENSOR = 1
+        LIGHT_SENSOR = 4;
+        %LEFT_ULTRASONIC = 2
+        LEFT_ULTRASONIC = 3;
+        %FRONT_ULTRASONIC = 4
+        FRONT_ULTRASONIC = 1;
+        %RIGHT_ULTRASONIC = 3
+        RIGHT_ULTRASONIC = 2;
     end
 end
